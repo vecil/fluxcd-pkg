fluxcd/pkg

refactor: move DependencyReference to apis/meta
Signed-off-by: Iam-karan-suresh <karansuresh.info@gmail.com>

runtime/dependency/sort.go
@@ -56,7 +56,11 @@ func Sort(objects []Dependent) ([]meta.NamespacedObjectReference, error) {
 			Namespace: obj.GetNamespace(),
 		}
 		vertices = append(vertices, u)
-		for _, v := range obj.GetDependsOn() {
+		for _, dep := range obj.GetDependsOn() {
+			v := meta.NamespacedObjectReference{
+				Name:      dep.Name,
+				Namespace: dep.Namespace,
+			}
 			if v.Namespace == "" {
 				v.Namespace = obj.GetNamespace()
 			}

vecil/fluxcd-pkg

runtime/dependency/sort.go
@@ -56,7 +56,11 @@ func Sort(objects []Dependent) ([]meta.NamespacedObjectReference, error) {
 			Namespace: obj.GetNamespace(),
 		}
 		vertices = append(vertices, u)
-		for _, v := range obj.GetDependsOn() {
+		for _, depRef := range obj.GetDependsOn() {
+			v := meta.NamespacedObjectReference{
+				Name:      depRef.Name,
+				Namespace: depRef.Namespace,
+			}
 			if v.Namespace == "" {
 				v.Namespace = obj.GetNamespace()
 			}